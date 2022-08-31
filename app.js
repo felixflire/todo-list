d1c402b8b7b8fdd5b55fe0d47900451688df7090
felixflire <felixflire3@gmail.com>
2022-08-31 16:54:12 +0300

    todo-list

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+var items=["buy food","coock food","eat food"];
+let workItems=[];
+
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
+
+  var options={
+    weekday: "long",
+    day:"numeric",
+    month:"long"
+  };
+
+  var day = today.toLocaleDateString("en-US",options);
+  
+
+  res.render("list", { listTitle: day, newListItems: items });
+});
+
+
+app.post("/",function(req,res){
+   var item= req.body.newItem;
+
+  
+   if (req.body.list==="work"){
+    workItems.push(item);
+    res.redirect("/work");
+   }
+   else{
+    items.push(item)
+    res.redirect("/");
+   }
+})
+app.get("/work",function(req,res){
+    res.render("list",{listTitle: "work List",newListItems: workItems});
+
+});
+app.post("/work",function(req,res){
+    let item=req.body.newItem;
+    workItems.push(item);
+    res.redirect("/");
+});
+app.get("/about",function(req,res){
+    res.render("about");
+})
+
+app.listen(3000, function () {
+  console.log("server started on port 3000");
+});
